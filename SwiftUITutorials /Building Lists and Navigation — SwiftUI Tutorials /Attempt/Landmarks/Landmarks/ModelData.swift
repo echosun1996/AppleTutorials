647676b08b944db204af7f4ffccdbc2d33cb8d10
System Administrator <root@Echo-Macbook.local>
2020-12-23 10:00:02 +0800

    Auto commit by script at [Wed Dec 23 10:00:00 CST 2020].

--- SwiftUITutorials /Building Lists and Navigation — SwiftUI Tutorials /Attempt/Landmarks/Landmarks/ModelData.swift
+++ SwiftUITutorials /Building Lists and Navigation — SwiftUI Tutorials /Attempt/Landmarks/Landmarks/ModelData.swift
@@ -5,16 +5,28 @@
 //  Created by EchoSun on 2020/12/22.
 //
 
-import SwiftUI
+import Foundation
 
-struct ModelData: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+var landmarkData: [Landmark] = load("landmarkData.json")
+
+func load<T: Decodable>(_ filename: String) -> T {
+    let data: Data
+
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+        else {
+            fatalError("Couldn't find \(filename) in main bundle.")
     }
-}
 
-struct ModelData_Previews: PreviewProvider {
-    static var previews: some View {
-        ModelData()
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
+    }
+
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
     }
 }
